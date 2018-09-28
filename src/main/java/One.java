ed48ad26ae74d6e2522b4a79ea720f2ae8a6ccf6
Evgeniy <evjesu@gmail.com>
2018-09-28 10:08:20 +0700

    Second step.

--- src/main/java/One.java
+++ src/main/java/One.java
@@ -1,5 +1,7 @@
 public class One {
     public static void main(String args[]){
         System.out.println("Hello!");
+        Two test = new Two(1,5);
+        System.out.println(test.getA()+" + "+test.getB()+" = "+test.sum());
     }
 }
---
+++ src/main/java/Two.java
@@ -0,0 +1,19 @@
+public class Two {
+    private int a,b;
+    Two(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sum(){
+        return (a+b);
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
