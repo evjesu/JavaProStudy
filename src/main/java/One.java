0081c502969d18fbaa1121952478ff6e8d50082a
Evgeniy <evjesu@gmail.com>
2018-10-04 03:12:28 +0700

    Practice 3

--- src/main/java/One.java
+++
@@ -1,7 +0,0 @@
-public class One {
-    public static void main(String args[]){
-        System.out.println("Hello!");
-        Two test = new Two(1,5);
-        System.out.println(test.getA()+" + "+test.getB()+" = "+test.sum());
-    }
-}
